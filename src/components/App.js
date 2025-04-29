87b3af6c92b78a1efe6b50a91eeb5f6486a5d4aa
ibrahim <your-email@example.com>
2025-04-29 10:08:33 +0300

    react-hooks-react-app

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,17 @@
-import React from "react";
 import { format } from "date-fns";
 import ExampleComponent from "./ExampleComponent";
 
-// Add your code own within the return statement
 function App() {
+  const currentTime = format(new Date(), "MMMM do yyyy, h:mm:ss a");
+
   return (
     <div className="App">
-      <h1>{format(new Date(), "MMMM do yyyy, h:mm:ss a")}</h1>
+      <h1>{currentTime}</h1>
       <p className="App-intro">
-        In React apps, we write JSX - it looks like HTML, and uses a lot of HTML
-        syntax. JSX lets us include JavaScript functions right along with the
-        HTML, and also allows us to add in components, which are separate,
-        self-contained chunks of JSX.
+        In React apps, we write JSX — it looks like HTML and uses a lot of HTML
+        syntax. JSX lets us include JavaScript functions right alongside the HTML
+        and also allows us to add components, which are separate, self-contained
+        chunks of JSX.
       </p>
       <ExampleComponent />
     </div>
@@ -19,3 +19,4 @@ function App() {
 }
 
 export default App;
+
